42495a35db8f44fe44bd298d54a694219248c573
bilouwan <moroine.bentefrit@adactive.com>
2016-03-14 21:27:53 +0100

    First working version

---
+++ fs-promise.js
@@ -0,0 +1,134 @@
+"use strict";
+
+import fs from 'fs.extra';
+
+export default {
+    F_OK: fs.F_OK,
+    R_OK: fs.R_OK,
+    W_OK: fs.W_OK,
+    X_OK: fs.X_OK,
+    copyRecursive: function (from, to) {
+        return new Promise((resolve, reject) => {
+            fs.copyRecursive(from, to, (err) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve();
+            });
+        });
+    },
+    rmrf: function (dir) {
+        return new Promise((resolve, reject) => {
+            fs.rmrf(dir, (err) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve();
+            });
+        });
+    },
+    rename: function (oldPath, newPath) {
+        return new Promise((resolve, reject) => {
+            fs.rename(oldPath, newPath, (err) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve();
+            });
+        });
+    },
+    writeFile: function (file, data, options) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(file, data, options, (err) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve();
+            });
+        });
+    },
+    readdir: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.readdir(path, (err, names) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve(names);
+            });
+        });
+    },
+    unlink: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.unlink(path, (err) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve();
+            });
+        });
+    },
+    createWriteStream: function (path, options) {
+        return fs.createWriteStream(path, options);
+    },
+    access: function (path, mode) {
+        return new Promise((resolve, reject) => {
+            fs.access(path, mode, (err) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve();
+            });
+        });
+    },
+    mkdirp: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.mkdirp(path, (err) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve();
+            });
+        });
+    },
+    readlink: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.readlink(path, (err, link) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve(link);
+            });
+        });
+    },
+    lstat: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.lstat(path, (err, stats) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve(stats);
+            });
+        });
+    },
+    stat: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.stat(path, (err, stats) => {
+                if (err) {
+                    reject(err);
+                }
+
+                resolve(stats);
+            });
+        });
+    }
+};
