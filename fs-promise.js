f555a529c705331150142e21240fb880c2ac7c55
Moroine Bentefrit <moroine.bentefrit@adactive.com>
2016-07-05 17:48:14 +0200

    Move require into the top
    Move require into the top in order to make it parsable by webpack

--- fs-promise.js
+++ fs-promise.js
@@ -1,159 +1,157 @@
 "use strict";
 
-module.exports = (() => {
-    let fs = require('fs.extra');
-    let mv = require("mv");
+let fs = require('fs.extra');
+let mv = require("mv");
 
-    return {
-        F_OK: fs.F_OK,
-        R_OK: fs.R_OK,
-        W_OK: fs.W_OK,
-        X_OK: fs.X_OK,
-        copyRecursive: function (from, to) {
-            return new Promise((resolve, reject) => {
-                fs.copyRecursive(from, to, (err) => {
-                    if (err) {
-                        reject(err);
-                    }
+module.exports = {
+    F_OK: fs.F_OK,
+    R_OK: fs.R_OK,
+    W_OK: fs.W_OK,
+    X_OK: fs.X_OK,
+    copyRecursive: function (from, to) {
+        return new Promise((resolve, reject) => {
+            fs.copyRecursive(from, to, (err) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve();
-                });
+                resolve();
             });
-        },
-        rmrf: function (dir) {
-            return new Promise((resolve, reject) => {
-                fs.rmrf(dir, (err) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    rmrf: function (dir) {
+        return new Promise((resolve, reject) => {
+            fs.rmrf(dir, (err) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve();
-                });
+                resolve();
             });
-        },
-        rename: function (oldPath, newPath) {
-            return new Promise((resolve, reject) => {
-                mv(oldPath, newPath, (err) => {
-                    if (err) {
-                        // Retry after 2 seconds (fs references may not been relaesed)
-                        setTimeout(() => {
-                            mv(oldPath, newPath, (err) => {
-                                if (err) {
-                                    reject(err);
-                                }
+        });
+    },
+    rename: function (oldPath, newPath) {
+        return new Promise((resolve, reject) => {
+            mv(oldPath, newPath, (err) => {
+                if (err) {
+                    // Retry after 2 seconds (fs references may not been relaesed)
+                    setTimeout(() => {
+                        mv(oldPath, newPath, (err) => {
+                            if (err) {
+                                reject(err);
+                            }
 
-                                resolve();
-                            });
-                        }, 10000);
+                            resolve();
+                        });
+                    }, 10000);
 
-                        return;
-                    }
+                    return;
+                }
 
-                    resolve();
-                });
+                resolve();
             });
-        },
-        writeFile: function (file, data, options) {
-            return new Promise((resolve, reject) => {
-                fs.writeFile(file, data, options, (err) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    writeFile: function (file, data, options) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(file, data, options, (err) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve();
-                });
+                resolve();
             });
-        },
-        readdir: function (path) {
-            return new Promise((resolve, reject) => {
-                fs.readdir(path, (err, names) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    readdir: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.readdir(path, (err, names) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve(names);
-                });
+                resolve(names);
             });
-        },
-        unlink: function (path) {
-            return new Promise((resolve, reject) => {
-                fs.unlink(path, (err) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    unlink: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.unlink(path, (err) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve();
-                });
+                resolve();
             });
-        },
-        createWriteStream: function (path, options) {
-            return fs.createWriteStream(path, options);
-        },
-        access: function (path, mode) {
-            return new Promise((resolve, reject) => {
-                fs.access(path, mode, (err) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    createWriteStream: function (path, options) {
+        return fs.createWriteStream(path, options);
+    },
+    access: function (path, mode) {
+        return new Promise((resolve, reject) => {
+            fs.access(path, mode, (err) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve();
-                });
+                resolve();
             });
-        },
-        mkdirp: function (path) {
-            return new Promise((resolve, reject) => {
-                fs.mkdirp(path, (err) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    mkdirp: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.mkdirp(path, (err) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve();
-                });
+                resolve();
             });
-        },
-        readlink: function (path) {
-            return new Promise((resolve, reject) => {
-                fs.readlink(path, (err, link) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    readlink: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.readlink(path, (err, link) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve(link);
-                });
+                resolve(link);
             });
-        },
-        lstat: function (path) {
-            return new Promise((resolve, reject) => {
-                fs.lstat(path, (err, stats) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    lstat: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.lstat(path, (err, stats) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve(stats);
-                });
+                resolve(stats);
             });
-        },
-        stat: function (path) {
-            return new Promise((resolve, reject) => {
-                fs.stat(path, (err, stats) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    stat: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.stat(path, (err, stats) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve(stats);
-                });
+                resolve(stats);
             });
-        },
-        readFile: function (path) {
-            return new Promise((resolve, reject) => {
-                fs.readFile(path, (err, data) => {
-                    if (err) {
-                        reject(err);
-                    }
+        });
+    },
+    readFile: function (path) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, (err, data) => {
+                if (err) {
+                    reject(err);
+                }
 
-                    resolve(data);
-                });
+                resolve(data);
             });
-        }
-    };
-})();
+        });
+    }
+};
